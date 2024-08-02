7cc5c43ff02ab0953a7230bfe32608cf9864844b
jonatas <delimajonatas875@gmail.com>
2024-08-02 16:38:32 -0300

    feat: get vehicles handler

--- internal/handler/vehicle_handler.go
+++ internal/handler/vehicle_handler.go
@@ -1,7 +1,22 @@
 package handler
 
-import "github.com/j0n4t45d3v/parking_management/internal/domain"
+import (
+	"fmt"
+	"net/http"
 
-func ListVehicles() ([]domain.Vehicle, int16, error) {
-  
+	"github.com/j0n4t45d3v/parking_management/internal/common"
+	"github.com/j0n4t45d3v/parking_management/internal/service"
+)
+
+func GetVehicles(res http.ResponseWriter, req *http.Request) {
+  result, status,err := service.GetAllVehicles()
+
+  if err != nil {
+    errorResponse := common.ToJsonError(int(status), err.Error())
+    fmt.Fprint(res, errorResponse)
+    return
+  }
+
+  response := common.ToJsonSucess(int(status), result)
+  fmt.Fprint(res, response)
 }
