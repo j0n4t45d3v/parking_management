bda356ac54d668c3bddfd8a083d19856bc2a9905
Jonatas <delimajonatas875@gmail.com>
2024-08-03 10:34:15 -0300

    feat: middlerware content-type application/json

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"log"
 	"net/http"
-  "log"
+
 	"github.com/gorilla/mux"
+	"github.com/j0n4t45d3v/parking_management/internal/middlerware"
 	"github.com/j0n4t45d3v/parking_management/internal/route"
 )
 
 func main() {
   muxRoute := mux.NewRouter()
+  muxRoute.Use(middlerware.ContentTypeMiddlerware)
   api := muxRoute.PathPrefix("/api").Subrouter()
   route.SetRouteV1Establishment(api)
   log.Println("Running Server...")
