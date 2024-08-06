f579ddc6bd81e91926c7e63a083badbecc02b9bc
jonatas <delimajonatas875@gmail.com>
2024-08-06 08:55:23 -0300

    style: format code

--- database/connection.go
+++ database/connection.go
@@ -8,26 +8,25 @@ import (
 )
 
 var connection *sql.DB
-	const (
-		host     = "127.0.0.1"
-		port     = 5432
-		user     = "postgres"
-		password = "senhasecreta"
-		dbname   = "postgres"
-	)
 
+const (
+	host     = "127.0.0.1"
+	port     = 5432
+	user     = "postgres"
+	password = "senhasecreta"
+	dbname   = "postgres"
+)
 
-func GetConnection() (*sql.DB, error){
+func GetConnection() (*sql.DB, error) {
 
-  if connection == nil {
-	  psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
-    connection,_ = sql.Open("postgres", psqlInfo)
-  }
+	if connection == nil {
+		psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+		connection, _ = sql.Open("postgres", psqlInfo)
+	}
 
-    if err := connection.Ping(); err != nil {
-    return nil, err
-  }
- 
+	if err := connection.Ping(); err != nil {
+		return nil, err
+	}
 
-  return connection, nil
+	return connection, nil
 }
